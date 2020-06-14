67d9369cc689a643f038e1d24db8f9fd9f0dbea9
Anderson "Yagasaki" Marlon <anderson18_marlon@hotmail.com>
2020-06-14 02:23:47 -0300

    Update to Prettier's Style

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 function ptbr() {
-    document.getElementById('story').innerHTML = '<a href="#container1">HISTÓRIA</a>';
-    document.getElementById('skills').innerHTML = '<a href="#container1">HABILIDADES</a>';
-    document.getElementById('story2').innerHTML = 'HISTÓRIA';
-    document.getElementById('storytext').innerHTML = 'Anderson Marlon, 21 anos, Campinas.<br><br>Graças a um curso de Web Design & Design Gráfico realizado em Novembro de 2013, <br> aflorei minha criatividade e ajudei a minha família com identidade visual, hoje, <br>tento aprimorar minhas habilidades e mostrá-las ao mundo que os problemas <br>simples como identidade visual podem ser resolvidos da melhor maneira possível e de <br>uma forma confiável, seja em necessidade de Design Gráfico ou até mesmo Front-End.<br><br>· Curso de Web Design / Design Gráfico, concluído em 11/2013.<br>· Certificado em Ilustração e Design Gráfico para Web da Bradesco em 05/2017.<br>· Certificado em Web Design: Dev in PHP, HTML, CSS e Javascript em 01/2019.<br>· Certificado em Programação Web para Divulgação Científica em 01/2019.<br><br>';
-    document.getElementById('skills2').innerHTML = 'HABILIDADES FRONT-END';
-    document.getElementById('skills3').innerHTML = 'HABILIDADES DESIGN';
-    document.getElementById('contact').innerHTML = 'CONTATO';
-    document.getElementById('copyright').innerHTML = 'DESENVOLVIDO POR ANDERSON "YAGASAKI" MARLON';
+  document.getElementById("story").innerHTML =
+    '<a href="#container1">HISTÓRIA</a>';
+  document.getElementById("skills").innerHTML =
+    '<a href="#container1">HABILIDADES</a>';
+  document.getElementById("story2").innerHTML = "HISTÓRIA";
+  document.getElementById("storytext").innerHTML =
+    "Anderson Marlon, 21 anos, Campinas.<br><br>Graças a um curso de Web Design & Design Gráfico realizado em Novembro de 2013, <br> aflorei minha criatividade e ajudei a minha família com identidade visual, hoje, <br>tento aprimorar minhas habilidades e mostrá-las ao mundo que os problemas <br>simples como identidade visual podem ser resolvidos da melhor maneira possível e de <br>uma forma confiável, seja em necessidade de Design Gráfico ou até mesmo Front-End.<br><br>· Curso de Web Design / Design Gráfico, concluído em 11/2013.<br>· Certificado em Ilustração e Design Gráfico para Web da Bradesco em 05/2017.<br>· Certificado em Web Design: Dev in PHP, HTML, CSS e Javascript em 01/2019.<br>· Certificado em Programação Web para Divulgação Científica em 01/2019.<br><br>";
+  document.getElementById("skills2").innerHTML = "HABILIDADES FRONT-END";
+  document.getElementById("skills3").innerHTML = "HABILIDADES DESIGN";
+  document.getElementById("contact").innerHTML = "CONTATO";
+  document.getElementById("copyright").innerHTML =
+    'DESENVOLVIDO POR ANDERSON "YAGASAKI" MARLON';
 }
 
 function enusa() {
-    document.getElementById('story').innerHTML = '<a href="#container1">STORY</a>';
-    document.getElementById('skills').innerHTML = '<a href="#container2">SKILLS</a>';
-    document.getElementById('story2').innerHTML = 'STORY';
-    document.getElementById('storytext').innerHTML = 'Anderson Marlon, 21, Campinas.<br><br>Thanks to a Web Design and Graphic Design course held in November 2013, <br>I became creative and helped my family with visual identity today, <br>I try to hone my skills and show them to the world that simple problems <br>like visual identity can be solved in the best possible way and <br>in a reliable way, whether in need of Graphic Design or even Front End.<br><br>· Course of Web Design / Graphic Design, completed on 11/2013.<br>· Certificate in Illustration and Graphic Design for Bradesco Web on 05/2017.<br>· Certificate in Web Design: Dev in PHP, HTML, CSS and JavaScript on 01/2019.<br>· Certificate in Web Programming for Scientific Dissemination on 01/2019.<br><br>';
-    document.getElementById('skills2').innerHTML = 'SKILLS FRONT-END';
-    document.getElementById('skills3').innerHTML = 'SKILLS DESIGN';
-    document.getElementById('contact').innerHTML = 'CONTACT';
-    document.getElementById('copyright').innerHTML = 'DEVELOPED BY ANDERSON “YAGASAKI” MARLON';
+  document.getElementById("story").innerHTML =
+    '<a href="#container1">STORY</a>';
+  document.getElementById("skills").innerHTML =
+    '<a href="#container2">SKILLS</a>';
+  document.getElementById("story2").innerHTML = "STORY";
+  document.getElementById("storytext").innerHTML =
+    "Anderson Marlon, 21, Campinas.<br><br>Thanks to a Web Design and Graphic Design course held in November 2013, <br>I became creative and helped my family with visual identity today, <br>I try to hone my skills and show them to the world that simple problems <br>like visual identity can be solved in the best possible way and <br>in a reliable way, whether in need of Graphic Design or even Front End.<br><br>· Course of Web Design / Graphic Design, completed on 11/2013.<br>· Certificate in Illustration and Graphic Design for Bradesco Web on 05/2017.<br>· Certificate in Web Design: Dev in PHP, HTML, CSS and JavaScript on 01/2019.<br>· Certificate in Web Programming for Scientific Dissemination on 01/2019.<br><br>";
+  document.getElementById("skills2").innerHTML = "SKILLS FRONT-END";
+  document.getElementById("skills3").innerHTML = "SKILLS DESIGN";
+  document.getElementById("contact").innerHTML = "CONTACT";
+  document.getElementById("copyright").innerHTML =
+    "DEVELOPED BY ANDERSON “YAGASAKI” MARLON";
 }
